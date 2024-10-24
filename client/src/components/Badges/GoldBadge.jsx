6659b393f664d5e5d737dde02899830a8ea2048d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-24 12:47:13 +0530

    Update

--- client/src/components/Badges/GoldBadge.jsx
+++ client/src/components/Badges/GoldBadge.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const GoldBadge = ({ Icon }) => {
   return (
-    <div>GoldBadge</div>
+    <div className="py-6 px-6 bg-gradient-to-tr from-amber-100 to-yellow-400 rounded-xl">
+        <center>{Icon}</center>
+    </div>
   )
 }
 
--- client/src/pages/Dashboard/DashHome.jsx
+++ client/src/pages/Dashboard/DashHome.jsx
@@ -3,6 +3,7 @@ import { GrUserNew } from "react-icons/gr";
 import { useNavigate } from 'react-router-dom';
 import secureLocalStorage from 'react-secure-storage'
 import DashBtns from '../../components/DashBtns/DashBtns';
+import GoldBadge from '../../components/Badges/GoldBadge';
 
 const DashHome = () => {
     const navigate = useNavigate()
@@ -14,9 +15,7 @@ const DashHome = () => {
             <div className="md:w-1/3 bg-[#292f45] md:mr-2 p-4 rounded-xl shadow-md md:mb-0 mb-2">
                 <h1 className="text-white/60 font-semibold text-center">Congratulations {EmailUser}</h1>
                   <div className="flex justify-center my-4">
-                    <div className="py-6 px-6 bg-gradient-to-tr from-amber-100 to-yellow-400 rounded-xl">
-                      <center><GrUserNew className='h-8 w-auto text-yellow-600'/></center>
-                    </div>
+                    <GoldBadge Icon={<GrUserNew className='h-8 w-auto text-yellow-600'/>} />
                   </div>
                 <h1 className="text-white/60  text-center">You Successfully earned Welcome Badge</h1>
 
@@ -28,7 +27,7 @@ const DashHome = () => {
                 <h1 className="text-white/60 font-semibold text-xl">Statistics</h1>
 
                 <div className="md:grid grid-cols-4 gap-2">
-                    <div className="">
+                    <div className="flex">
                         
                     </div>
                 </div>
