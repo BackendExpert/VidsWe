4ec50af581472a008653d997308292232e2cb187
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-10-24 11:15:56 +0530

    Update DashFooter.jsx

--- client/src/components/Footers/DashFooter.jsx
+++ client/src/components/Footers/DashFooter.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DashFooter = () => {
   return (
-    <div className='md:mx-0 mx-2 text-white/60'>CopyRight &copy; 2024 | Developed and Maintained by <a href="" target='_blank' className='text-[#9e94f5] font-semibold'>JehanKandy</a> | All rights Reserved</div>
+    <div className='md:mx-0 mx-2 text-white/60'>CopyRight &copy; 2024 | Developed and Maintained by <a href="https://github.com/BackendExpert" target='_blank' className='text-[#9e94f5] font-semibold'>JehanKandy</a> | All rights Reserved</div>
   )
 }
 
